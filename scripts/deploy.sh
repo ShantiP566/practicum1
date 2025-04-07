497b49f965cd866ec1a7895f332fceddd58e8c8b
shanti-jangam <shantir.jangam@gmail.com>
2025-04-07 16:05:13 -0400

    deploy.sh changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,3 +16,8 @@ kubectl apply -f "$TMP_FILE"
 
 # Clean up
 rm "$TMP_FILE"
+
+kubectl create secret generic postgres-primes-secrets \
+  --from-literal=DATASOURCE_URL="jdbc:postgresql://your-db-host:5432/primes" \
+  --from-literal=DATASOURCE_USERNAME="admin2024" \
+  --from-literal=DATASOURCE_PASSWORD="p4662024!"
